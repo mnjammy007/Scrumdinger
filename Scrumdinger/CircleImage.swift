b7a3cdea895554bc573ac8085660b5f9d8997db1
mnjammy007 <naseem4954r@gmail.com>
2025-01-23 12:52:39 +0530

    chapter 1 completed

---
+++ Scrumdinger/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Scrumdinger
+//
+//  Created by Apple on 23/01/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- Scrumdinger/ContentView.swift
+++ Scrumdinger/ContentView.swift
@@ -9,12 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, Nasir")
-                .font(.title)
-                .foregroundColor(.green)
+        VStack{
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack{
+                    Text("Joshua Tree National Park")
+                    
+                    Spacer()
+                    
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text")
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
