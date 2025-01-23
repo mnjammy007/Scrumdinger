ed84fc47222e5992f9a018d1b11666fc0f24257d
mnjammy007 <naseem4954r@gmail.com>
2025-01-23 18:32:27 +0530

    done upto drawing paths and shape

---
+++ Scrumdinger/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Scrumdinger
+//
+//  Created by Apple on 23/01/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
